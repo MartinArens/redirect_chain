af31f3681d5dd062f1cd85cbc0e1909b8fb1994f
arens <arens@sipgate.de>
2018-08-17 10:46:34 +0200

    init

--- redirect_chain.py
+++ redirect_chain.py
@@ -1,1 +1,29 @@
+#!/usr/bin/env python
+
+import requests
+import sys
+
+url = sys.argv[1]
+
+
+def get_redirect_chain(url):
+    try:
+        r = requests.get(url)
+
+        if len(r.history) > 0:
+            chain = ""
+            code = r.history[0].status_code
+            final = r.url
+            for resp in r.history:
+                chain += resp.url + " | "
+            return str(code) + ' - ' + str(len(r.history)) + ' - ' + chain + ' - ' + final + ' - '
+        else:
+            return str(r.status_code)
+    except requests.ConnectionError:
+        print("Error")
+        return '0'
+
+
+print(get_redirect_chain(url))
+
 
